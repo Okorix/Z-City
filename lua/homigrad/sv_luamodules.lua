5ccf187e4088dfe4676af598e7855ff9ddd0e05d
Okorix <HZZVvv@outlook.com>
2026-08-03 17:53:45 +0400

    move lua modules to sv_luamodules.lua

---
+++ lua/homigrad/sv_luamodules.lua
@@ -0,0 +1,51 @@
+local luaModules = {
+	tbl = {
+		eightbit = {
+			name = "eightbit",
+			startFunc = function()
+				if system.IsLinux() then
+					print("If the eightbit module doesn't work, you should update the 32-bit glibc library (and C/C++ related 32-bit libraries in general)")
+				end
+
+				require("eightbit")
+
+				if eightbit.SetDamp1 then
+					eightbit.SetDamp1(0.85)
+				end
+
+				if eightbit.SetProotCutoff then
+					eightbit.SetProotCutoff(0.7)
+				end
+
+				if eightbit.SetProotGain then
+					eightbit.SetProotGain(0.7)
+				end
+			end,
+			notInstalledFunc = function()
+				MsgC(Color(255, 0, 0), "Eightbit module is not found! Install it to keep voice effects.\n")
+			end
+		},
+		datadesc = {
+			name = "datadesc",
+			notinstalledfunc = function()
+				MsgC(Color(255, 0, 0), "Datadesc module is not found! Install it to keep replacements fully working.\n")
+			end
+		}
+	}
+}
+
+for i,v in pairs(luaModules.tbl) do
+	local moduleName = v.name
+	local moduleStartFunction = v.startFunc
+	local moduleNotInstalledFunction = v.notInstalledFunc
+	if util.IsBinaryModuleInstalled(moduleName) then
+		if not v.noAutoRequire then
+			require(moduleName)
+		end
+		if moduleStartFunction ~= nil then
+			moduleStartFunction()
+		end
+	else
+		moduleNotInstalledFunction()
+	end
+end
